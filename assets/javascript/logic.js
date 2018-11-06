ab5f4e5a9dc9b6c6ea612ad26a942dcb94b6a9e5
ScottE <scotteratigan@gmail.com>
2018-11-05 19:16:56 -0800

    It speaks

---
+++ assets/javascript/logic.js
@@ -0,0 +1,6 @@
+$('#speak-entered-text').on('click', function() {
+    event.preventDefault();
+    let memeText = $("#meme-text-to-speak").val().trim();
+    //alert(memeText);
+    speak(memeText);
+});
